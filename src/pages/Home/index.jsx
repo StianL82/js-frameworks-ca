47734ebc8a9c6864d869d65a666fd7081d9fc599
StianL82 <stian.lilleng@gmail.com>
2024-11-26 14:06:30 +0100

    Added product cards to home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,20 +1,35 @@
+// Home index.jsx
 import React from "react";
 import * as S from "./index.styles";
-import { Link } from "react-router-dom";
+import useFetchAPI from "../../hooks/FetchAPI";
+import ProductGrid from "../../components/ProductGrid";
 
 function Home() {
+  const { data, isLoading, isError } = useFetchAPI(
+    "https://v2.api.noroff.dev/online-shop/"
+  );
+
+  if (isLoading) {
+    return <S.Paragraph>Loading products...</S.Paragraph>;
+  }
+
+  if (isError) {
+    return <S.Paragraph>Error fetching products!</S.Paragraph>;
+  }
+
+  const products = data?.data || [];
+
   return (
-    <div>
-      <div>
-        <S.Heading>Welcome to My Homepage</S.Heading>
-        <S.Paragraph>
-          This is a test to see if all styles and components are working
-          correctly.
-        </S.Paragraph>
-        <S.Button as={Link} to="/product/1" isActive>
-            Product Page
-          </S.Button>
-      </div>
+    <div className="container">
+      <S.Heading>Welcome to My Homepage</S.Heading>
+      <S.Paragraph>
+        This is a test to see if all styles and components are working correctly.
+      </S.Paragraph>
+      {products.length > 0 ? (
+        <ProductGrid products={products} />
+      ) : (
+        <S.Paragraph>No products available</S.Paragraph>
+      )}
     </div>
   );
 }
