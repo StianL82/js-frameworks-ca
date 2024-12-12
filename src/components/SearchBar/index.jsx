acb194b7c85dbd2e85dd60bb2fc7244969215cbb
StianL82 <stian.lilleng@gmail.com>
2024-12-12 13:51:16 +0100

    Added icon to searchbar

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
+import { FaSearch } from "react-icons/fa";
 import * as S from "./index.styles";
 
 function SearchBar({ onSearch }) {
   const [searchTerm, setSearchTerm] = useState("");
-  
+
   useEffect(() => {
     const debounceTimeout = setTimeout(() => {
       onSearch(searchTerm);
@@ -19,14 +20,19 @@ function SearchBar({ onSearch }) {
   };
 
   return (
-    <S.Input
-      type="text"
-      placeholder="Search for products..."
-      value={searchTerm}
-      onChange={handleInputChange}
-    />
+    <S.SearchContainer>
+      <FaSearch className="search-icon" />
+      <S.Input
+        type="text"
+        placeholder="Search for products..."
+        value={searchTerm}
+        onChange={handleInputChange}
+      />
+    </S.SearchContainer>
   );
 }
 
 export default SearchBar;
+
+
 
