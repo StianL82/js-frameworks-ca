1e59374dc97042c64c81f93e2f71824a05e0dd2d
StianL82 <stian.lilleng@gmail.com>
2024-12-17 14:07:44 +0100

    Added some tests

---
+++ src/components/CartIcon/index.test.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { ThemeProvider } from "styled-components"; 
+import CartIcon from "./index";
+
+jest.mock("../CartContext", () => ({
+  useCart: () => ({
+    cart: [],
+  }),
+}));
+
+jest.mock("react-router-dom", () => ({
+  Link: ({ children, to }) => <a href={to}>{children}</a>,
+}));
+
+const theme = {
+  color: {
+    dangerColor: "#ff0000",
+    white: "#ffffff",
+  },
+};
+
+describe("CartIcon", () => {
+  test("renders Cart Icon and shows 0 items when cart is empty", () => {
+    render(
+      <ThemeProvider theme={theme}>
+        <CartIcon />
+      </ThemeProvider>
+    );
+
+    const cartImage = screen.getByAltText("Cart Icon");
+    expect(cartImage).toBeInTheDocument();
+    expect(cartImage).toHaveAttribute("src", "/images/cart_icon.svg");
+    expect(screen.getByText("0")).toBeInTheDocument();
+  });
+});
+
+
+
---
+++ src/components/CheckoutButton/index.test.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import CheckoutButton from "./index";
+import { useCart } from "../../components/CartContext";
+import { useNavigate } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+
+jest.mock("../../components/CartContext", () => ({
+  useCart: jest.fn(),
+}));
+
+jest.mock("react-router-dom", () => ({
+  useNavigate: jest.fn(),
+}));
+
+const theme = {
+  color: {
+    white: "#FFFFFF",
+    primary: "#007BFF",
+    danger: "#FF0000",
+  },
+};
+
+describe("CheckoutButton", () => {
+  test("renders Checkout button", () => {
+    const mockClearCart = jest.fn();
+    useCart.mockReturnValue({ clearCart: mockClearCart });
+    const mockNavigate = jest.fn();
+    useNavigate.mockReturnValue(mockNavigate);
+
+    render(
+      <ThemeProvider theme={theme}>
+        <CheckoutButton />
+      </ThemeProvider>
+    );
+
+    expect(screen.getByText("Continue to Checkout")).toBeInTheDocument();
+  });
+
+  test("calls clearCart and navigates to CheckoutSuccess on click", () => {
+    const mockClearCart = jest.fn();
+    useCart.mockReturnValue({ clearCart: mockClearCart });
+    const mockNavigate = jest.fn();
+    useNavigate.mockReturnValue(mockNavigate);
+
+    render(
+      <ThemeProvider theme={theme}>
+        <CheckoutButton />
+      </ThemeProvider>
+    );
+
+    const button = screen.getByText("Continue to Checkout");
+    fireEvent.click(button);
+
+    expect(mockClearCart).toHaveBeenCalled();
+    expect(mockNavigate).toHaveBeenCalledWith("/CheckoutSuccess");
+  });
+});
+
